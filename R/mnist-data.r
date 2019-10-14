02fe083b630f2cf866540e634eb787b6c093de5e
Huan Chen <hzchenhuan@gmail.com>
2019-10-14 17:31:01 -0400

    Add mnist data into package

---
+++ R/mnist-data.r
@@ -0,0 +1,12 @@
+#' MNIST database (Modified National Institute of Standards and Technology database)
+#'
+#' A large database of handwritten digits that is commonly used for training various image processing systems.
+#'
+#' @docType data
+#'
+#' @usage data(mnist)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(mnist)
